6511cdd3e261d8a449dd8fd02909596476e34779
danielsarmento <danielsarmento2@hotmail.com>
2023-06-16 15:46:57 -0300

    ajuste timer

--- src/server.js
+++ src/server.js
@@ -10,12 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-cron.schedule('0-59 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
+cron.schedule('0-59/30 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
   let hora = new Date
   console.log(`Executando Disparos... ${hora.getHours()}:${hora.getMinutes().toString().padStart(2, '0')} | ${hora.getDate().toString().padStart(2, '0')}/${(hora.getMonth() +1).toString().padStart(2, '0')}/${hora.getFullYear()}`)
   axios.get(`http://145.14.134.34:3333/routine`)
 })
 
 app.listen(3333, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3333');
 });
