759c95c6251806defa974c0fb59129fa1534c7ee
danielsarmento <danielsarmento2@hotmail.com>
2023-07-02 13:54:59 -0300

    ajuste julho

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-cron.schedule('0-59/5 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
+cron.schedule('0-59/5 0-23 1-31 7 mon-tue-wed-thu-fri-sat-sun', () => {
   let hora = new Date
   console.log(`Executando Disparos... ${hora.getHours()}:${hora.getMinutes().toString().padStart(2, '0')} | ${hora.getDate().toString().padStart(2, '0')}/${(hora.getMonth() +1).toString().padStart(2, '0')}/${hora.getFullYear()}`)
   axios.get(`http://145.14.134.34:3333/routine`)
