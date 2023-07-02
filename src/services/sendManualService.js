8dbac5f83bc1920c1ca3ca6e3649f2f05d5f49da
danielsarmento <danielsarmento2@hotmail.com>
2023-07-01 23:00:26 -0300

    servidor-pc

--- src/services/sendManualService.js
+++ src/services/sendManualService.js
@@ -17,14 +17,14 @@ export class SendManualService {
     
     async send (name, videoLink, phone) {
         //const link = videoLink.replace(" ", "")
-        try {
-            /* await axios.post('https://api.zenvia.com/v2/channels/whatsapp/messages', {
+          try {
+              /*await axios.post('https://api.zenvia.com/v2/channels/whatsapp/messages', {
                 from: `558398679409`,
                 to: `55${phone}`,
                 contents: [
                     {
                         type: 'template',
-                        templateId: `989a319d-e069-44bd-8252-e7adfca41378`,
+                        templateId: `6d5d5264-764d-4df2-86dc-d47ca8a45e51`,
                         fields: {
                             name: `${name}`,
                             videoLink: `${link}`
@@ -36,7 +36,7 @@ export class SendManualService {
                     'X-API-TOKEN': `onmngqVKP0U9KNbsiE3yY-Z86A7b6U4w5CXy`,
                     'Content-Type': 'application/json',
                 },
-            }); */
+            });*/
             await axios.post('https://api.zenvia.com/v2/flow-batches', {
             flowId: `b4656ce4-07ab-41c5-9329-3c0203b8664f`,
             name: 'Fluxo Ativo - Dilis São João',
--- src/services/sendRoutineService.js
+++ src/services/sendRoutineService.js
@@ -41,15 +41,15 @@ export class SendRoutineService {
     }
 
     async send (name, videoLink, phone) {
-        //const link = videoLink.replace(" ", "")
-        try {
-            /* await axios.post('https://api.zenvia.com/v2/channels/whatsapp/messages', {
+       //const link = videoLink.replace(" ", "")
+         try {
+              /*await axios.post('https://api.zenvia.com/v2/channels/whatsapp/messages', {
                 from: `558398679409`,
                 to: `55${phone}`,
                 contents: [
                     {
                         type: 'template',
-                        templateId: `989a319d-e069-44bd-8252-e7adfca41378`,
+                        templateId: `6d5d5264-764d-4df2-86dc-d47ca8a45e51`,
                         fields: {
                             name: `${name}`,
                             videoLink: `${link}`
@@ -61,7 +61,7 @@ export class SendRoutineService {
                     'X-API-TOKEN': `onmngqVKP0U9KNbsiE3yY-Z86A7b6U4w5CXy`,
                     'Content-Type': 'application/json',
                 },
-            }); */
+            });*/
             await axios.post('https://api.zenvia.com/v2/flow-batches', {
             flowId: `b4656ce4-07ab-41c5-9329-3c0203b8664f`,
             name: 'Fluxo Ativo - Dilis São João',
